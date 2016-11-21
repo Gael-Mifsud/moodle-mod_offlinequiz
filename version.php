0ce02b804d906a26df0d5d526ed7593537fe1578
Markus Kemmerling <markus.kemmerling@meduniwien.ac.at>
2016-11-21 10:50:40 +0100

    update version 2016051802

--- version.php
+++ version.php
@@ -30,8 +30,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2016051801;
-$plugin->release   = "v3.0.0";      // User-friendly version number.
+
+$plugin->version  = 2016051802;
+$plugin->release   = "v3.0.2";      // User-friendly version number.
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires = 2015111600 ;         // Requires this Moodle version.
 $plugin->cron     = 3600;               // Period for cron to check this plugin (secs).
